4d19818e5e8a7a58b429a05051431834dae050db
zzero23 <devji0ng23@gmail.com>
2025-07-17 21:44:54 +0900

    Title: 실수 받아 그대로 출력, Time: 142ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250717/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,12 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        // 출력
+        System.out.printf("%.2f", num1);
+    }
+}
