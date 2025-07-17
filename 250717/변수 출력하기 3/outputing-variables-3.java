759b6a187cdd6265b33b7699d3c9a4c89e48e5f5
zzero23 <devji0ng23@gmail.com>
2025-07-17 21:11:42 +0900

    Title: 변수 출력하기 3, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250717/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+
+        // 출력
+        System.out.println(a + "->" + b + "->" + c);
+    }
+}
