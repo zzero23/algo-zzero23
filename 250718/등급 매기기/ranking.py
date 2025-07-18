72a30ae7f9feeebb5a401b7ef2777f6fc9518c04
zzero23 <devji0ng23@gmail.com>
2025-07-18 22:28:38 +0900

    Title: 등급 매기기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250718/등급 매기기/ranking.py
@@ -0,0 +1,14 @@
+# 입력
+score = int(input())
+
+# 조건문
+if score >= 90 :
+    print("A")
+elif score >= 80 :
+    print("B")
+elif score >= 70 :
+    print("C")
+elif score >= 60 :
+    print("D")
+else :
+    print("F")
