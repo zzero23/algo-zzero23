6140c014cbc3a8e949c52c274533074e7452028c
zzero23 <devji0ng23@gmail.com>
2025-07-18 22:32:10 +0900

    Title: 등급 매기기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250718/등급 매기기/ranking.java
@@ -0,0 +1,23 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        // 조건문
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
