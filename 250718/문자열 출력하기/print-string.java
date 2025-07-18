3489e8e54a47e060dcb087ba8e1674de6900a6fa
zzero23 <devji0ng23@gmail.com>
2025-07-18 22:44:57 +0900

    Title: 문자열 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250718/문자열 출력하기/print-string.java
@@ -0,0 +1,15 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+
+        // 반복문
+        for (int i = 0; i < count; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
