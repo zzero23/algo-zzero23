3038af3314233b8cb608cec2e6a254f25d86f064
zzero23 <devji0ng23@gmail.com>
2025-07-18 22:54:41 +0900

    Title: 차례로 출력, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250718/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 반복문
+        for (int i = 5; i < 18; i++) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
