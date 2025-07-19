81152d454a8ceaf248f44cb01de5a279230315f9
zzero23 <devji0ng23@gmail.com>
2025-07-20 00:42:20 +0900

    Title: 특정 수 맞히기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250720/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,11 @@
+intN = int(input())
+
+# 반복 및 조건문
+while intN != 25 :
+    intN = int(input())
+    if intN < 25 :
+        print("Lower")
+    else :
+        print("Higher")
+if intN == 25 :
+    print("Good")
