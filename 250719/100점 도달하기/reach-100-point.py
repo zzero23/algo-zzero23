bbc1861c81827ba151cbaa6d0f9b302a0d2566f3
zzero23 <devji0ng23@gmail.com>
2025-07-19 22:41:24 +0900

    Title: 100점 도달하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250719/100점 도달하기/reach-100-point.py
@@ -0,0 +1,15 @@
+# 입력
+intScore = int(input())
+
+# 반복 및 조건문
+for i in range(intScore, 101, 1) :
+    if i >= 90 :
+        print("A", end = " ")
+    elif i >= 80 :
+        print("B", end = " ")
+    elif i >= 70 :
+        print("C", end = " ")
+    elif i >= 60 :
+        print("D", end = " ")
+    else :
+        print("E", end = " ")
