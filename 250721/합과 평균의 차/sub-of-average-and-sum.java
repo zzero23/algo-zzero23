720bb654898851863e544cf42ed7fa42f87d74e4
zzero23 <devji0ng23@gmail.com>
2025-07-21 02:08:37 +0900

    Title: 합과 평균의 차, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,20 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intA = sc.nextInt();
+        int intB = sc.nextInt();
+        int intC = sc.nextInt();
+
+        // 연산
+        int intPlus = intA + intB + intC;
+        int intAverage = intPlus/3;
+        int intMinus = intPlus - intAverage;
+
+        // 출력
+        System.out.printf("%d\n%d\n%d", intPlus, intAverage, intMinus);
+    }
+}
