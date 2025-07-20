4d651b2329d0c9433f943061734740a18b64c50e
zzero23 <devji0ng23@gmail.com>
2025-07-21 00:40:10 +0900

    Title: 문자 받아 출력, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,13 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        char char1 = sc.next().charAt(0);
+
+        // 출력 
+        System.out.println(char1);
+    }
+}
