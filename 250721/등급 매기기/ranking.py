0fd95c48356ab925a4a11db3beebf0cd26bf9b3e
zzero23 <devji0ng23@gmail.com>
2025-07-21 03:09:13 +0900

    Title: 등급 매기기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/등급 매기기/ranking.py
@@ -0,0 +1,14 @@
+# 입력
+intN = int(input())
+
+# 조건문
+if intN >= 90 :
+    print("A")
+elif intN >= 80 :
+    print("B")
+elif intN >= 70 :
+    print("C")
+elif intN >= 60 :
+    print("D")
+else :
+    print("F")
