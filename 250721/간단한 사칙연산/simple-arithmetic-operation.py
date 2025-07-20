40e253de5692027c12a1f46842b487493f92691a
zzero23 <devji0ng23@gmail.com>
2025-07-21 01:22:04 +0900

    Title: 간단한 사칙연산, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,11 @@
+# 입력
+intA, intB = map(int, input().split())
+
+# 연산
+intPlus = intA + intB
+intMinus = intA - intB
+intShare = intA // intB
+intRemain = intA % intB
+
+# 출력
+print(f"{intPlus}\n{intMinus}\n{intShare}\n{intRemain}")
