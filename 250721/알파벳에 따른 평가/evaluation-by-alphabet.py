bca6c7575bf551cd0409c075da5fbf1baa250eec
zzero23 <devji0ng23@gmail.com>
2025-07-21 03:14:20 +0900

    Title: 알파벳에 따른 평가, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+# 입력
+char1 = input()
+
+# 조건문
+if char1 == "S" :
+    print("Superior")
+elif char1 == "A" :
+    print("Excellent")
+elif char1 == "B" :
+    print("Good")
+elif char1 == "C" :
+    print("Usually")
+elif char1 == "D" :
+    print("Effort")
+else :
+    print("Failure")
