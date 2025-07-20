a3d10066f9db4b47cc24d27592747c31646c57b6
zzero23 <devji0ng23@gmail.com>
2025-07-20 23:51:46 +0900

    Title: 입력받아 계산, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250720/입력받아 계산/input-calculate.java
@@ -0,0 +1,16 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intA = sc.nextInt();
+
+        // 연산
+        int intResult = intA + 2;
+
+        // 출력 
+        System.out.println(intResult);
+    }
+}
