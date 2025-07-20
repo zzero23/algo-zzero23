4eb57d91e522b6204c161668846d9570729eaee8
zzero23 <devji0ng23@gmail.com>
2025-07-21 00:26:11 +0900

    Title: 입력받아 출력, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/입력받아 출력/input-and-output.java
@@ -0,0 +1,14 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intA = sc.nextInt();
+        int intB = sc.nextInt();
+
+        // 출력
+        System.out.printf("%d %d", intA, intB);
+    }
+}
