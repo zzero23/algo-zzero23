c9516f473663c35a722e61b0b8b908c21c86ae02
zzero23 <devji0ng23@gmail.com>
2025-07-20 23:54:13 +0900

    Title: 점수 입출력, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250720/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,13 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intScore = sc.nextInt();
+
+        // 출력
+        System.out.printf("Your score is %d point.", intScore);
+    }
+}
