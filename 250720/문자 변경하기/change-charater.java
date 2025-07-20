68f7a8bb5f2608457264edd7f962ccac7b15ca22
zzero23 <devji0ng23@gmail.com>
2025-07-20 23:19:44 +0900

    Title: 문자 변경하기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/문자 변경하기/change-charater.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의 1
+        char charA = 'C';
+
+        // 변수 정의 2
+        charA = 'T';
+
+        // 출력
+        System.out.println(charA);
+    }
+}
