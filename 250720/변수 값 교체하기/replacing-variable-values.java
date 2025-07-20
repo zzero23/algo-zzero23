f4ea68a8394495206a7f1803cbfd7fcce3ab895c
zzero23 <devji0ng23@gmail.com>
2025-07-20 23:16:12 +0900

    Title: 변수 값 교체하기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/변수 값 교체하기/replacing-variable-values.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의 1
+        int intA = 3;
+
+        // 변수 정의 2
+        intA = 6;
+
+        // 출력
+        System.out.println(intA);
+    }
+}
