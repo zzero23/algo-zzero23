ae8ac79f45e3a6f3a0423a4fcb2227f871d3076d
zzero23 <devji0ng23@gmail.com>
2025-07-21 01:31:47 +0900

    Title: 두수의 곱과 몫, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
@@ -0,0 +1,10 @@
+# 입력
+intA, intB = map(int, input().split())
+
+# 연산
+intMulti = intA * intB
+intShare = intA // intB
+
+# 출력
+print(f"{intA} * {intB} = {intMulti}")
+print(f"{intA} / {intB} = {intShare}")
