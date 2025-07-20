66d0371146fe4606e09df5c33b87035b6591b690
zzero23 <devji0ng23@gmail.com>
2025-07-20 23:14:23 +0900

    Title: 두 실수의 곱, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        double dblA = 5.26, dblB = 8.27;
+
+        // 연산
+        double dblResult = dblA * dblB;
+
+        // 출력
+        System.out.printf("%.3f", dblResult);
+    }
+}
