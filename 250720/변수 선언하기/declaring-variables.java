64dd81d350af8edf6103f46cdf80b2d8383934e6
zzero23 <devji0ng23@gmail.com>
2025-07-20 22:33:23 +0900

    Title: 변수 선언하기, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/변수 선언하기/declaring-variables.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        int intA = 3;
+        char charB = 'C';
+
+        // 출력
+        System.out.println(intA);
+        System.out.println(charB);
+    }
+}
