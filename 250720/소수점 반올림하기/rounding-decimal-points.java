7b674ccfc3d613e92bd5bc32ffc73b8cc3c34948
zzero23 <devji0ng23@gmail.com>
2025-07-20 22:57:05 +0900

    Title: 소수점 반올림하기, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        double dblA = 25.352;
+
+        // 출력
+        System.out.printf("%.1f", dblA);
+    }
+}
