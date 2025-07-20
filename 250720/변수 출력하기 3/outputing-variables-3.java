8db54e5f1577676c95b22fe149042f520a872cd2
zzero23 <devji0ng23@gmail.com>
2025-07-20 22:47:11 +0900

    Title: 변수 출력하기 3, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        int intA = 1, intB = 2;
+        char charC = 'C';
+
+        // 출력
+        System.out.printf("%d->%d->%s", intA, intB, charC);
+    }
+}
