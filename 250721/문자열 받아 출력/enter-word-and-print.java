4b0b5f275192f3338eb740f87aa35e0e1c639bb0
zzero23 <devji0ng23@gmail.com>
2025-07-21 00:42:13 +0900

    Title: 문자열 받아 출력, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,13 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+
+        // 출력 
+        System.out.println(str1);
+    }
+}
