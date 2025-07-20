8c60d519f35ebbc94390376a85b792311451da47
zzero23 <devji0ng23@gmail.com>
2025-07-21 03:22:58 +0900

    Title: 살 수 있는 물건 2, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,21 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intN = sc.nextInt();
+
+        // 조건문
+        if (intN >= 3000) {
+            System.out.println("book");
+        } else if (intN >= 1000) {
+            System.out.println("mask");
+        } else if (intN >= 500) {
+            System.out.println("pen");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
