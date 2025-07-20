12e3c3376a52251a4e4cc2da771dffed5c0da57c
zzero23 <devji0ng23@gmail.com>
2025-07-21 02:47:15 +0900

    Title: 삼항연산자, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/삼항연산자/ternary-operator.java
@@ -0,0 +1,16 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intScore = sc.nextInt();
+
+        // 조건문
+        String strResult = intScore == 100 ? "pass" : "failure";
+
+        // 출력
+        System.out.println(strResult);
+    }
+}
