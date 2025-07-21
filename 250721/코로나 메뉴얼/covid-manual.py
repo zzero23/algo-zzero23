23e2cc7b7bf410485fc3aa15fa4917f038d7500e
zzero23 <devji0ng23@gmail.com>
2025-07-21 23:01:19 +0900

    Title: 코로나 메뉴얼, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/코로나 메뉴얼/covid-manual.py
+++ 250721/코로나 메뉴얼/covid-manual.py
@@ -5,12 +5,12 @@ strState3, strTemp3 = input().split()
 
 # 조건문
 if strState1 == "Y" and int(strTemp1) >= 37 :
-    if (strState2 == "Y" and int(strTemp2) >= 27) or (strState3 == "Y" and int(strTemp3) >= 27) :
+    if (strState2 == "Y" and int(strTemp2) >= 37) or (strState3 == "Y" and int(strTemp3) >= 37) :
         print("E")
     else :
         print("N")
 else :
-    if (strState2 == "Y" and int(strTemp2) >= 27) and (strState3 == "Y" and int(strTemp3) >= 27) :
+    if (strState2 == "Y" and int(strTemp2) >= 37) and (strState3 == "Y" and int(strTemp3) >= 37) :
         print("E")
     else :
         print("N")
