686184705aa7b475a964c5917e71d8f7fb1b22e9
zzero23 <devji0ng23@gmail.com>
2025-07-21 18:28:05 +0900

    Title: 시력 검사, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/시력 검사/eye-test.py
@@ -0,0 +1,11 @@
+# 입력
+dblA = float(input())
+dblB = float(input())
+
+# 조건문
+if dblA >= 1.0 and dblB >= 1.0 :
+    print("High")
+elif dblA >= 0.5 and dblB >= 0.5 :
+    print("Middle")
+else : 
+    print("Low")
