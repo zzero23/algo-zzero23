cfb21ff3db871a589407650bc43d387fc9668c43
zzero23 <devji0ng23@gmail.com>
2025-07-21 10:00:53 +0900

    Title: 두 번의 연산, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/두 번의 연산/two-operations.java
@@ -0,0 +1,22 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intA = sc.nextInt();
+
+        // 조건문
+        if (intA % 2 != 0) {
+            intA += 3;
+        }
+
+        if (intA % 3 == 0) {
+            intA /= 3;
+        }
+
+        // 출력
+        System.out.println(intA);
+    }
+}
