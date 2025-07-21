1c42cd994f2c0cd9430d79f3cba5ecda91a27879
zzero23 <devji0ng23@gmail.com>
2025-07-21 12:12:01 +0900

    Title: 4가지 관계연산자, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,15 @@
+# 입력
+intA, intB = map(int, input().split())
+
+# 조건문
+intResult = "1" if intA >= intB else "0"
+print(intResult)
+
+intResult = "1" if intA > intB else "0"
+print(intResult)
+
+intResult = "1" if intB >= intA else "0"
+print(intResult)
+
+intResult = "1" if intB > intA else "0"
+print(intResult)
