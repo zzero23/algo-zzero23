4bc3aaaf39b163266774ba59ca52b136fa4808b1
zzero23 <devji0ng23@gmail.com>
2025-07-21 21:55:50 +0900

    Title: 수의 계절은, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/수의 계절은/season-of-num.py
@@ -0,0 +1,11 @@
+# 입력
+intM = int(input())
+
+if intM == 3 or intM == 4 or intM == 5 :
+    print("Spring")
+elif intM == 6 or intM == 7 or intM == 8 :
+    print("Summer")
+elif intM == 9 or intM == 10 or intM == 11 :
+    print("Fall")
+else :
+    print("Winter")
