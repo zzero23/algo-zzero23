b8ff2332974aec4a2ce6e50c62d1b1262730ee48
zzero23 <devji0ng23@gmail.com>
2025-07-21 11:18:17 +0900

    Title: 비교 연산, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/비교 연산/comparison-operator.py
@@ -0,0 +1,24 @@
+# 입력 
+intA, intB = map(int, input().split())
+
+# 변수 정의
+intResult = 0
+
+# 조건문
+intResult = 1 if (intA >= intB) else 0
+print(intResult)
+
+intResult = 1 if(intA > intB) else 0
+print(intResult)
+
+intResult = 1 if(intB >= intA) else 0
+print(intResult)
+
+intResult = 1 if(intB > intA) else 0;
+print(intResult)
+
+intResult = 1 if(intA == intB) else 0;
+print(intResult)
+
+intResult = 1 if(intA != intB) else 0;
+print(intResult)
