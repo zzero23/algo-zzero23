cae1cb2275d61ca4d6becdd0aab1130dbdf81286
zzero23 <devji0ng23@gmail.com>
2025-07-22 20:46:43 +0900

    Title: N까지 자연수 출력, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250722/N까지 자연수 출력/print-1-to-n.java
@@ -0,0 +1,19 @@
+// import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int intN = sc.nextInt();
+
+        // 변수 정의
+        int intCount = 1;
+
+        // 출력
+        while (intCount < intN + 1) {
+            System.out.print(intCount + " ");
+            intCount++;
+        }
+    }
+}
