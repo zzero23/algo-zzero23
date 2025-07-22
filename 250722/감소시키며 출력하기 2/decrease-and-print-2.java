4188d32202e8dd06a09c348620e7c4ca4034eee2
zzero23 <devji0ng23@gmail.com>
2025-07-22 21:05:18 +0900

    Title: 감소시키며 출력하기 2, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250722/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 정의
+        int intN = 26;
+
+        // 출력
+        while (intN > 9) {
+            System.out.print(intN + " ");
+            intN--;
+        }
+    }
+}
